efa4a8ddb034d7b856cba1a2674fd6fa62cb0602
eomjinyoung <jinyoung.eom@gmail.com>
2019-01-03 11:37:56 +0900

    .

--- java-basic/src/main/java/ch02/Test01.java
+++ java-basic/src/main/java/ch02/Test01.java
@@ -8,40 +8,50 @@ public class Test01 {
     System.out.println(78);
     
     // => 8진수
+    System.out.println(0116);
     
     // => 16진수
+    System.out.println(0x4e);
     
     // => 2진수
+    System.out.println(0b01001110);
     
     // 2) 부동소수점을 출력하라.
     // => 12.345
+    System.out.println(12.345);
     
     // => 1.2345 를 출력할 때 12.345가 나오게 하라
+    System.out.println(1.2345e1);
     
     // => 0.12345 를 출력할 때 12.345가 나오게 하라
+    System.out.println(0.12345e2);
     
     // => 123.45  를 출력할 때 12.345가 나오게 하라
-    
+    System.out.println(123.45e-1);
     
     // 3) 논리값을 출력하라
     // => 참 을 출력하라
+    System.out.println(true);
     
     // => 거짓 을 출력하라
-    
+    System.out.println(false);
     
     // 4) 문자를 출력하라.
     // => 0x61 값을 출력했을 때 a가 나오게 하라.
+    System.out.println((char)0x61);
     
     // => 97 값을 출력했을 때 a가 나오게 하라.
+    System.out.println((char)97);
     
     // => 작은 따옴표를 사용하여 a를 출력하라.
-    
+    System.out.println('a');
     
     // 5) 문자열을 출력하라.
     // => Hello 문자열을 출력하라.
+    System.out.println("Hello");
     
     // => 안녕하세요 문자열을 출력하라.
-    
+    System.out.println("안녕하세요");
    
   }
 }
