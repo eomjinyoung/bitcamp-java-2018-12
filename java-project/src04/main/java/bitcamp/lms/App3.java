212a8ea3e5badd87bba26e47ef7508ca7740b89a
eomjinyoung <jinyoung.eom@gmail.com>
2019-01-04 17:40:26 +0900

    .

---
+++ java-project/src04/main/java/bitcamp/lms/App3.java
@@ -0,0 +1,11 @@
+package bitcamp.lms;
+
+public class App3 {
+
+  public static void main(String[] args) {
+    System.out.printf("번호: %d\n", 1);
+    System.out.printf("내용: %s\n", "게시글입니다.");
+    System.out.printf("작성일: %s\n", "2019-01-01");
+    System.out.printf("조회수: %d\n", 0);
+  }
+}
