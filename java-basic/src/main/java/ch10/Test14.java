9fd2777dbb99418b5bc7225e687bb8e6f4954e49
eomjinyoung <jinyoung.eom@gmail.com>
2019-01-15 15:13:01 +0900

    .

--- java-basic/src/main/java/ch10/Test14.java
+++ java-basic/src/main/java/ch10/Test14.java
@@ -25,20 +25,34 @@ public class Test14 {
     // 한글이 깨진다. 이유?
     // => String 생성자는 파라미터로 받은 바이트 배열에 ISO-8859-1 문자 코드가 들어 있다가 간주한다.
     //    즉 0xb0 0xa1 값이 한글 '가'가 아니라 0xb0와 0xa1 각각을 영어라 간주하고 
-    //    ISO-8859-1 에 정의된 문자표에 따라 화면에 출력한다.
-    //    0xb0에 해당하는 문자가 출력되는 것이다.
-    // => 제대로 한글을 처리하려면? 생성자에 바이트 배열을 넘겨줄 때 배열에 들어 있는 코드 값이 
-    //    어떤 문자표의 코드 값인지 알려줘야 한다.
+    //    ISO-8859-1 에 정의된 문자표에 따라 유니코드(UTF-16)으로 바꿔 저장한다.
+    //    0xb0(ISO-8859-1) ==> 0x00b0(UTF-16) 
+    // => 제대로 한글을 처리하려면? 
+    //    생성자에 바이트 배열을 넘겨줄 때 
+    //    배열에 들어 있는 코드 값이 어떤 문자표의 코드 값인지 알려줘야 한다.
     // 
     
     String s5 = new String(bytes, "euc-kr");
     
+    byte[] bytes2 = {(byte)0xac, (byte)0x00, (byte)0xac, (byte)0x01, 
+        0x00, 0x61, 0x00, 0x62, 0x00, 0x63};
     
+    // 바이트 배열에 들어 있는 코드는 무슨 문자표로 작성했는지 정확하게 알려줘야 한다.
+    // 그래야 자바의 문자 코드로 제대로 변경할 수 있을 것이다.
+    String s6 = new String(bytes2, "utf-16");
+    
+    byte[] bytes3 = {(byte)0xea, (byte)0xb0, (byte)0x80, 
+        (byte)0xea, (byte)0xb0, (byte)0x81, 
+        0x61, 0x62, 0x63};
+    
+    String s7 = new String(bytes3, "utf-8");
     
     System.out.println(s2);
     System.out.println(s3);
     System.out.println(s4);
     System.out.println(s5);
+    System.out.println(s6);
+    System.out.println(s7);
   }
 }
 
