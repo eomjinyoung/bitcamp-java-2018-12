986fc607a4c7aa832289712c361cf5916de5caeb
eomjinyoung <jinyoung.eom@gmail.com>
2019-01-30 13:20:11 +0900

    .

--- java-basic/src/main/java/ch21/f/Test03.java
+++ java-basic/src/main/java/ch21/f/Test03.java
@@ -1,19 +1,21 @@
 // try-with-resources 문법 : 문법 확인
 package ch21.f;
 
-import java.io.PrintWriter;
-import java.io.StringWriter;
-import java.util.Scanner;
-
 public class Test03 {
   
   static class MyResource1 {
+    public int divide(int a, int b) {
+      return a / b;
+    }
     public void close() {
       System.out.println("MyResource1.close()");
     }
   }
   
   static class MyResource2 implements AutoCloseable {
+    public int divide(int a, int b) {
+      return a / b;
+    }
     @Override
     public void close() {
       System.out.println("MyResource2.close()");
@@ -22,60 +24,24 @@ public class Test03 {
   
   public static void main(String[] args) {
     
-    // 자바에서는 자원 해제를 자동화시키는 try ~ catch 문법을 제공한다.
-    // 개발자가 finally에서 자원 해제시키는 코드를 작성할 필요가 없다.
-    // 단, java.lang.AutoCloseable 구현체인 경우에만 해당한다.
-    // 사용법
-    //    try (java.lang.AutoCloseable 구현체의 변수를 선언) {
-    //      ...
-    //    } catch (...) {
-    //      ...
-    //    }
-    //
-    try (Scanner keyboard = new Scanner(System.in);){
-
-      System.out.print("값? ");
-      int a = Integer.parseInt(keyboard.nextLine());
-      
-      int result = sum(a);
-      System.out.println(result);
-      
-    } catch (Exception e) {
-      
-      try ( // 이렇게 객체를 이 괄호에서 준비하면 자동으로 close()를 호출해 주기 때문에 
-            // 개발자는 코딩하기 편하다.
-          StringWriter out = new StringWriter();
-          PrintWriter out2 = new PrintWriter(out);
-      ) {
+    try (
+        // java.lang.AutoCloseable 을 구현하지 않은 객체는 선언할 수 없다.
+        //MyResource1 r1 = new MyResource1(); // 컴파일 오류!
+        //String str = "Hello"; // 컴파일 오류!
         
-        e.printStackTrace(out2);
-
-        // StringWriter 객체에 저장된 문자열 꺼내기
-        String str = out.toString();
-        System.out.println(str);
-        
-      } catch (Exception e2) {
-        // 자원을 해제시키다가 예외가 발생할 경우 개발자가 처리할 게 없다.
-        // 그래서 예외를 받기만 하고 특별히 뭔가를 수행하지는 않는다.
-      }
+        // java.lang.AutoCloseable 을 구현한 클래스는 객체를 선언할 수 있다.
+        MyResource2 r2 = new MyResource2(); // OK! 
+    ) {
+      System.out.println(r2.divide(10, 2));
+      System.out.println(r2.divide(10, 0));
       
+      // try 블록을 벗어나기 전에 close()가 자동 호출된다.
       
-    } finally {
-      // try-with-resources 문법을 사용하면 
-      // JVM이 try () 에 선언한 레퍼런스에 대해 try 블록을 벗어나기 전에 close()를 자동호출하기 때문에
-      // 다음과 같이 개발자가 직접 close()를 호출하는 코드를 작성할 필요가 없다.
-      //keyboard.close();
+    } catch (Exception e){
+      System.out.println("예외 발생!");
     }
-    
+     
   }
-  
-  static int sum(int value) { // f(n) = n + f(n-1)
-    if (value == 1)
-      return 1;
-    return value + sum(value - 1);
-  }
-  
- 
 }
 
 
