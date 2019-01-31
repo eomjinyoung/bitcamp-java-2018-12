f608543bc0add08a022191ce3582182a8e1108ed
eomjinyoung <jinyoung.eom@gmail.com>
2019-01-31 17:15:04 +0900

    .

---
+++ java-basic/src/main/java/ch22/b/Test01.java
@@ -0,0 +1,57 @@
+// 바이너리 데이터 입출력 - FileOutputStream 클래스 사용법
+// 
+package ch22.b;
+
+import java.io.FileOutputStream;
+
+public class Test01 {
+  public static void main(String[] args) {
+    // 바이너리 데이터?
+    // => character set(문자표) 규칙에 따라 작성한 파일이 아닌 파일.
+    // => 메모장으로 편집이 불가능한 파일.
+    // => 보통 전용 프로그램을 이용해서 편집하는 파일.
+    // => 예) .hwp, .gif, .jpeg, .mp3, .mp4, .ppt, .xls, .doc,
+    //       .class, .swf, .avi 등
+    // 
+    // 텍스트 데이터?
+    // => 특정 character set(문자표) 규칙에 따라 작성한 파일.
+    // => 메모장으로 편집 가능.
+    // => 예) .txt, .c, .cpp, .java, .xml, .html, .css, .js,
+    //       .py, .docx, .xlsx, .pptx 등
+    //
+    // Java I/O API(java.io 패키지)
+    // => 바이너리 입출력 : InputStream, OutputStream
+    // => 텍스트 입출력 : Reader, Writer
+    //
+    
+    // 파일로 바이너리 데이터 출력
+    // => FileOutputStream 클래스를 사용한다.
+    try {
+      // 1) 데이터를 출력을 담당할 객체를 생성한다.
+      FileOutputStream out = new FileOutputStream("data.bin");
+      
+      // 2) 데이터를 출력한다.
+      out.write(2);
+      out.write(40);
+      out.write(100);
+      out.write(101);
+      out.write(102);
+      out.write(127);
+      
+      out.close();
+      
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    
+    System.out.println("출력 완료!");
+  }
+}
+
+
+
+
+
+
+
+
---
+++ java-basic/src/main/java/ch22/b/Test02.java
@@ -0,0 +1,53 @@
+// 바이너리 데이터 입출력 - FileInputStream 클래스 사용법
+// 
+package ch22.b;
+
+import java.io.FileInputStream;
+
+public class Test02 {
+  public static void main(String[] args) {
+    
+    // 파일의 바이너리 데이터 읽기
+    // => FileInputStream 클래스를 사용한다.
+    try {
+      // 1) 데이터를 읽기를 담당할 객체를 생성한다.
+      FileInputStream in = new FileInputStream("data.bin");
+      
+      // 2) 데이터를 읽는다.
+      // => 읽은 데이터를 저장할 변수를 선언한다.
+      // => 1바이트 값을 저장할 변수는 int 형으로 선언한다. byte 타입이 아니다!
+      // => 이유? 0 ~ 255까지의 값을 읽기 때문이다.
+      //    byte는 -128 ~ 127까지의 값만 저장한다.
+      // => 여러 바이트를 읽을 때는 바이트 배열을 사용한다.
+      int b; 
+      /*
+      while (true) {
+        b = in.read();
+        if (b == -1) // 파일의 끝에 도달하면 -1을 리턴한다.
+          break;
+        System.out.print(Integer.toHexString(b) + " ");
+      }
+      */
+      while ((b = in.read()) != -1) {
+        System.out.print(Integer.toHexString(b) + " ");
+      }
+      
+      System.out.println();
+      
+      in.close();
+      
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    
+    System.out.println("출력 완료!");
+  }
+}
+
+
+
+
+
+
+
+
