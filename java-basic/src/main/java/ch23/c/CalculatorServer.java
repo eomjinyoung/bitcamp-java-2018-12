20a035e3489b6e6604ad98957061ddd4c1efd099
eomjinyoung <jinyoung.eom@gmail.com>
2019-02-11 11:55:58 +0900

    .

--- java-basic/src/main/java/ch23/c/CalculatorServer.java
+++ java-basic/src/main/java/ch23/c/CalculatorServer.java
@@ -1,11 +1,7 @@
-// 계산기 서버 만들기 - 3단계: 연산자 처리
+// 계산기 서버 만들기 - 7단계: 소스 코드 리팩토링 II - 클래스 분리하기
 package ch23.c;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.PrintStream;
 import java.net.ServerSocket;
-import java.net.Socket;
 
 public class CalculatorServer {
   public static void main(String[] args) {
@@ -14,32 +10,11 @@ public class CalculatorServer {
       System.out.println("서버 실행 중...");
       
       while (true) {
-        try (Socket socket = serverSocket.accept();
-            BufferedReader in = new BufferedReader(
-                new InputStreamReader(socket.getInputStream()));
-            PrintStream out = new PrintStream(socket.getOutputStream());) {
-          
-          String[] input = in.readLine().split(" ");
-          
-          int a = Integer.parseInt(input[0]);
-          String op = input[1];
-          int b = Integer.parseInt(input[2]);
-          int result = 0;
-          
-          switch (op) {
-            case "+": result = a + b; break;
-            case "-": result = a - b; break;
-            case "*": result = a * b; break;
-            case "/": result = a / b; break;
-            case "%": result = a % b; break;
-            default:
-              out.printf("%s 연산자를 지원하지 않습니다.\n", op);
-              out.flush();
-              continue;
-          }
-          
-          out.printf("결과는 %d 입니다.\n", result);
-          out.flush();
+        try {
+          new CalculatorProcessor(serverSocket.accept()).execute();
+        } catch (Exception e) {
+          System.out.println("클라이언트와 통신 중에 오류 발생!");
+          e.printStackTrace();
         }
       }
       
@@ -48,3 +23,15 @@ public class CalculatorServer {
     }
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
