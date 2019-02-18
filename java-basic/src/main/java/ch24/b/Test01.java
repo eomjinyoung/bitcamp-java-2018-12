094d9242f966c45955adc2fdfb42b65ce73f1d7d
eomjinyoung <jinyoung.eom@gmail.com>
2019-02-18 16:41:35 +0900

    .

--- java-basic/src/main/java/ch24/b/Test01.java
+++ java-basic/src/main/java/ch24/b/Test01.java
@@ -8,6 +8,13 @@ public class Test01 {
     // 스레드를 생성하여 main()을 호출한다.
     // 이렇게 main()을 호출하는 스레드를 "main" 스레드라 부른다.
 
+    // 현재 코드를 실행하는 스레드를 알아내기
+    Thread currThread = Thread.currentThread();
+    System.out.println(currThread.getName());
+    
   }
 
 }
+
+// JVM의 스레드 계층도:
+// main(T)
