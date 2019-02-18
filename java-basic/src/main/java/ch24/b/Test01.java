6fd1e2305ef93969a420977e975ee50277e78e73
eomjinyoung <jinyoung.eom@gmail.com>
2019-02-18 13:11:11 +0900

    .

---
+++ java-basic/src/main/java/ch24/b/Test01.java
@@ -0,0 +1,13 @@
+// "main" 스레드
+package ch24.b;
+
+public class Test01 {
+
+  public static void main(String[] args) {
+    // JVM은 main() 메서드를 호출할 때 직접 실행하지 않고 
+    // 스레드를 생성하여 main()을 호출한다.
+    // 이렇게 main()을 호출하는 스레드를 "main" 스레드라 부른다.
+
+  }
+
+}
