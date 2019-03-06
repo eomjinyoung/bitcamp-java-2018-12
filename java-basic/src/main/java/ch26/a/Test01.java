e3d2ae1670ed44cf1f4732c7dd7e3613756435af
eomjinyoung <jinyoung.eom@gmail.com>
2019-03-06 11:53:07 +0900

    .:

--- java-basic/src/main/java/ch26/a/Test01.java
+++ java-basic/src/main/java/ch26/a/Test01.java
@@ -8,21 +8,71 @@
 //    - mybatis.org 사이트에서 문서 페이지를 참조한다.
 //    - Mybatis 설정 파일(예: mybatis-config.xml)을 생성한다.
 //    - 문서 페이지를 참조하여 설정 파일의 내용을 변경한다.
+// 3) SQL 매퍼 파일 준비
+//    - mybatis.org 사이트에서 문서 페이지를 참조한다.
+//    - SQL이 들어 있는 파일(예: BoardMapper.xml)을 생성한다.
+//    - 문서 페이지를 참조하여 매퍼 파일의 내용을 변경한다.
+// 4) select 결과를 저장할 클래스 정의
+//    - ch26.a.Board 클래스 생성
+//    - 필드는 컬럼의 이름과 같게 한다.
+// 5) select SQL을 실행
+//    - mybatis 문서 페이지를 참조한다.
+//    - 관련 객체를 생성하고 사용하여 SQL을 실행한다.
+//
 package ch26.a;
 
+import java.io.FileInputStream;
 import java.io.InputStream;
 import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 public class Test01 {
 
   public static void main(String[] args) throws Exception {
-    String resource = "org/mybatis/example/mybatis-config.xml";
-    InputStream inputStream = Resources.getResourceAsStream(resource);
+    
+    // 1) mybatis 설정 파일을 읽을 때 사용할 도구를 준비한다.
+    // => 파일 입력 스트림 객체 준비
+    // => FileInputStream 객체를 직접 생성해도 되지만,
+    //    직접 생성하려면 설정 파일의 전체 경로를 정확하게 지정해야 한다.
+    //    예) c:/Users/user/git/bitcamp-2018-12/java-basic/src/main/java/ch26/a/mybatis-config.xml
+    //    만약 경로가 바뀌면 소스의 경로를 다시 변경해야 하는 문제가 있다.
+    // => mybatis 설정 파일이 자바 클래스 파일(.class)처럼 CLASSPATH에 있다면 
+    //    Resources.getResourceAsStream()을 호출하여 입력 스트림을 준비하는 것이 더 편하다.
+    // 
+    InputStream inputStream = Resources.getResourceAsStream(
+        "ch26/a/mybatis-config.xml");
+    
+    // 만약 Resources.getResourceAsStream()을 사용하지 않는다면 다음과 같이 
+    // 직접 FileInputStream을 생성해야 한다.
+    InputStream inputStream2 = new FileInputStream(
+        "/Users/eomjinyoung/git/bitcamp-java-2018-12/java-basic/src/main/java/ch26/a/mybatis-config.xml");
+    
+    // 2) mybatis 설정 파일에 따라 동작할 SQL 실행 도구를 준비한다.
+    // => SqlSessionFactoryBuilder 
+    //    - SqlSessionFactory 객체를 만들어 주는 일.
+    //    - Builder 디자인 패턴이 적용되어 있다.
+    //      (여러 객체를 조립하여 한 객체를 생성하는 경우, 직접 하는 대신에 
+    //       그 일을 해주는 객체 '빌더'를 통해 생성하는 것이 유지보수에 좋다.)
+    //
+    // => SqlSessionFactory 
+    //    - SqlSession 객체를 만들어 주는 일.
+    //    - Factory Method 디자인 패턴이 적용되어 있다.
+    //      (객체 생성 과정이 복잡할 경우 메서드를 통해 객체를 생성하는 것이 유지보수에 좋다.)
+    // 
+    // => SqlSession : SQL 매퍼 파일에 보관된 SQL을 찾아 실행해주는 일.
     SqlSessionFactory sqlSessionFactory =
       new SqlSessionFactoryBuilder().build(inputStream);
 
+    // 팩토리 메서드를 통해 객체를 생성한다.
+    SqlSession sqlSession = sqlSessionFactory.openSession();
   }
 
 }
+
+
+
+
+
+
---
+++ java-basic/src/main/java/design_pattern/builder/Engine.java
@@ -0,0 +1,40 @@
+package design_pattern.builder;
+
+public class Engine {
+  String model;
+  String cc;
+  int cylinder;
+  int valve;
+  
+  @Override
+  public String toString() {
+    return "Engine [model=" + model + ", cc=" + cc + ", cylinder=" + cylinder + ", valve=" + valve
+        + "]";
+  }
+  public String getModel() {
+    return model;
+  }
+  public void setModel(String model) {
+    this.model = model;
+  }
+  public String getCc() {
+    return cc;
+  }
+  public void setCc(String cc) {
+    this.cc = cc;
+  }
+  public int getCylinder() {
+    return cylinder;
+  }
+  public void setCylinder(int cylinder) {
+    this.cylinder = cylinder;
+  }
+  public int getValve() {
+    return valve;
+  }
+  public void setValve(int valve) {
+    this.valve = valve;
+  }
+  
+  
+}
