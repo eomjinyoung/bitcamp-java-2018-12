58f29bc27ab5948afc79e253200be7b900f84aa7
eomjinyoung <jinyoung.eom@gmail.com>
2019-03-07 17:19:53 +0900

    .

--- java-basic/src/main/java/ch26/h/Test01.java
+++ java-basic/src/main/java/ch26/h/Test01.java
@@ -1,7 +1,8 @@
-// dynamic SQL 다루기 - <sql> 태그 사용법
+// 조인 데이터 다루기 - join 사용 전
 package ch26.h;
 
 import java.io.InputStream;
+import java.util.List;
 import java.util.Scanner;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
@@ -13,7 +14,7 @@ public class Test01 {
   public static void main(String[] args) throws Exception {
     
     InputStream inputStream = Resources.getResourceAsStream(
-        "ch26/g/mybatis-config.xml");
+        "ch26/h/mybatis-config.xml");
     SqlSessionFactory sqlSessionFactory =
       new SqlSessionFactoryBuilder().build(inputStream);
     
@@ -21,11 +22,20 @@ public class Test01 {
  
     Scanner keyboard = new Scanner(System.in);
     System.out.print("게시물 번호? ");
-    String keyword = keyboard.nextLine();
+    int no = Integer.parseInt(keyboard.nextLine());
     keyboard.close();
     
-    Board board = sqlSession.selectOne("board.select1", Integer.parseInt(keyword));
+    // 조인을 사용하지 않고 자식 테이블의 데이터를 조회하려면 
+    // 다음과 같이 여러 번 SQL을 실행해야 한다.
+    //
+    Board board = sqlSession.selectOne("board.selectBoard", no);
     System.out.println(board);
+    
+    List<AttachFile> files = sqlSession.selectList("board.selectAttachFile", no);
+    System.out.println("[첨부파일]");
+    for (AttachFile f : files) {
+      System.out.println("  => " + f);
+    }
     System.out.println("-------------------------------");
     
   }
