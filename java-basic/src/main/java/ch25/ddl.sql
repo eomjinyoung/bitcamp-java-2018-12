f7e0941fdd07149f42a11518f97ed70c4fcdf4d1
eomjinyoung <jinyoung.eom@gmail.com>
2019-03-07 16:46:59 +0900

    .

--- java-basic/src/main/java/ch25/ddl.sql
+++ java-basic/src/main/java/ch25/ddl.sql
@@ -1,5 +1,9 @@
 -- 게시판 테이블 삭제
-drop table x_board;
+drop table x_board if exists x_board restrict;
+
+-- 게시물 첨부파일 테이블 삭제
+drop table x_board_file if exists x_board_file restrict;
+
 
 -- 게시판 테이블 생성
 create table x_board (
@@ -8,6 +12,14 @@ create table x_board (
   contents text null,
   created_date datetime null default now(),
   view_count int null default 0
+);
+
+-- 게시물 첨부파일 테이블 생성
+create table x_board_file (
+  board_file_id int not null primary key auto_increment,
+  file_path varchar(255) not null,
+  board_id int not null,
+  constraint fk_board_file foreign key (board_id) references x_board(board_id)
 );
 
 -- 데이터 입력 
