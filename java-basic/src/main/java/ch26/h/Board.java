37cf342787098fced2af6ba33e97ae7ca8ea33cb
eomjinyoung <jinyoung.eom@gmail.com>
2019-03-07 18:23:42 +0900

    .

--- java-basic/src/main/java/ch26/h/Board.java
+++ java-basic/src/main/java/ch26/h/Board.java
@@ -1,6 +1,7 @@
 package ch26.h;
 
 import java.sql.Date;
+import java.util.List;
 
 public class Board {
   
@@ -9,6 +10,12 @@ public class Board {
   private String contents;
   private Date createdDate;
   private int viewCount;
+  
+  // 자식 테이블(x_board_file)의 데이터를 여러 개 받을 필드를 선언한다.
+  private List<AttachFile> attachFiles;
+  
+  // 만약 자식 테이블의 데이터를 한 개만 받는다면 다음과 같이 선언하면 된다.
+  //private AttachFile attachFile;
   
   @Override
   public String toString() {
@@ -46,6 +53,11 @@ public class Board {
   public void setViewCount(int viewCount) {
     this.viewCount = viewCount;
   }
-  
+  public List<AttachFile> getAttachFiles() {
+    return attachFiles;
+  }
+  public void setAttachFiles(List<AttachFile> attachFiles) {
+    this.attachFiles = attachFiles;
+  }
   
 }
