f7d17fdb4f032e9d881a9b7cf77d96fe71e9478f
eomjinyoung <jinyoung.eom@gmail.com>
2019-03-08 12:49:15 +0900

    .

--- java-project2-server/src/main/java/com/eomcs/lms/dao/mariadb/LessonDaoImpl.java
+++ java-project2-server/src/main/java/com/eomcs/lms/dao/mariadb/LessonDaoImpl.java
@@ -1,19 +1,13 @@
 package com.eomcs.lms.dao.mariadb;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.util.List;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import com.eomcs.lms.dao.LessonDao;
 import com.eomcs.lms.domain.Lesson;
-import com.eomcs.util.DataSource;
 
 public class LessonDaoImpl implements LessonDao {
 
-  // DataSource 의존 객체 선언
-  DataSource dataSource;
-  
   // Mybatis 의존 객체 선언
   SqlSessionFactory sqlSessionFactory;
   
@@ -41,43 +35,18 @@ public class LessonDaoImpl implements LessonDao {
   }
 
   public int update(Lesson lesson) {
-    Connection con = dataSource.getConnection();
-    
-    try (PreparedStatement stmt = con.prepareStatement(
-        "update lms_lesson set"
-        + " titl = ?,"
-        + " conts = ?,"
-        + " sdt = ?,"
-        + " edt = ?,"
-        + " tot_hr = ?,"
-        + " day_hr = ?"
-        + " where lesson_id = ?")) {
-      
-      stmt.setString(1, lesson.getTitle());
-      stmt.setString(2, lesson.getContents());
-      stmt.setDate(3, lesson.getStartDate());
-      stmt.setDate(4, lesson.getEndDate());
-      stmt.setInt(5, lesson.getTotalHours());
-      stmt.setInt(6, lesson.getDayHours());
-      stmt.setInt(7, lesson.getNo());
-      
-      return stmt.executeUpdate();
-    } catch (Exception e) {
-      throw new RuntimeException(e);
+    try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
+      int count = sqlSession.update("LessonMapper.update", lesson);
+      sqlSession.commit();
+      return count;
     }
   }
 
   public int delete(int no) {
-    Connection con = dataSource.getConnection();
-    
-    try (PreparedStatement stmt = con.prepareStatement(
-        "delete from lms_lesson where lesson_id = ?")) {
-      
-      stmt.setInt(1, no);
-      
-      return stmt.executeUpdate();
-    } catch (Exception e) {
-      throw new RuntimeException(e);
+    try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
+      int count = sqlSession.delete("LessonMapper.delete", no);
+      sqlSession.commit();
+      return count;
     }
   }
 }
--- java-project2-server/src/main/java/com/eomcs/lms/handler/LessonUpdateCommand.java
+++ java-project2-server/src/main/java/com/eomcs/lms/handler/LessonUpdateCommand.java
@@ -22,11 +22,14 @@ public class LessonUpdateCommand extends AbstractCommand {
       return;
     }
     
-    Lesson temp = lesson.clone();
+    // 변경할 값만 temp에 저장할 것이기 때문에 기존 데이터를 복제하지 않는다. 
+    Lesson temp = new Lesson();
+    temp.setNo(no);
     
+    // mybatis는 필드의 값이 null이 아니거나, 숫자인 경우 0인 아니면 해당 컬럼을 값을 update 한다.
     String input = response.requestString(String.format(
         "수업명(%s)?", lesson.getTitle()));
-    if (input.length() > 0) 
+    if (input.length() > 0)
       temp.setTitle(input);
     
     input = response.requestString(String.format(
@@ -54,8 +57,18 @@ public class LessonUpdateCommand extends AbstractCommand {
     if (input.length() > 0)
       temp.setDayHours(Integer.parseInt(input));
     
-    lessonDao.update(temp);
-     
-    response.println("변경했습니다.");
+    if (temp.getTitle() != null
+        || temp.getContents() != null
+        || temp.getStartDate() != null
+        || temp.getEndDate() != null
+        || temp.getTotalHours() > 0
+        || temp.getDayHours() > 0) {
+      
+      lessonDao.update(temp);
+      response.println("변경했습니다.");
+      
+    } else {
+      response.println("변경 취소했습니다.");
+    }
   }
 }
