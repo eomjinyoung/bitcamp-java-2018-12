1d4277b15ec8d541b374d8bb7da807760d5c5150
eomjinyoung <jinyoung.eom@gmail.com>
2019-03-25 13:11:27 +0900

    .

--- java-project2-server/src/main/java/com/eomcs/lms/handler/BoardCommand.java
+++ java-project2-server/src/main/java/com/eomcs/lms/handler/BoardCommand.java
@@ -37,12 +37,21 @@ public class BoardCommand {
   }
   
   @RequestMapping("/board/add")
-  public void add(ServletResponse response) throws Exception {
+  public void add(ServletRequest request, ServletResponse response) throws Exception {
     Board board = new Board();
-    board.setContents(response.requestString("내용?"));
+    board.setContents(request.getParameter("contents"));
+    
     boardService.add(board);
     
-    response.println("저장하였습니다.");
+    PrintWriter out = response.getWriter();
+    out.println("<html><head>"
+        + "<title>게시물 등록</title>"
+        + "<meta http-equiv='Refresh' content='1;url=/board/list'>"
+        + "</head>");
+    out.println("<body><h1>게시물 등록</h1>");
+    out.println("<p>저장하였습니다.</p>");
+    out.println("</body></html>");
+    
   }
   
   @RequestMapping("/board/detail")
@@ -61,49 +70,72 @@ public class BoardCommand {
       return;
     }
     
+    out.println("<form action='/board/update'>");
     out.println("<table border='1'>");
+    out.printf("<tr>"
+        + "<th>번호</th>"
+        + "<td><input type='text' name='no' value='%d' readonly></td>"
+        + "</tr>\n", no);
     out.println(String.format(
-        "<tr> <th>내용</th> <td>%s</td> </tr>", board.getContents()));
+        "<tr> <th>내용</th> "
+        + "<td><textarea name='contents' rows='3' cols='50'>%s</textarea></td> "
+        + "</tr>", board.getContents()));
     out.println(String.format(
         "<tr> <th>작성일</th> <td>%s</td> </tr>", board.getCreatedDate()));
     out.println(String.format(
         "<tr> <th>조회수</th> <td>%d</td> </tr>", board.getViewCount()));
     
     out.println("</table>");
-    out.println("<p><a href='/board/list'>목록</a><p>");
+    out.println("<p><a href='/board/list'>목록</a>"
+        + " <a href='/board/delete?no=" + board.getNo() + "'>삭제</a>"
+        + " <button type='submit'>변경</button>"
+        + "<p>");
+    out.println("</form>");
     out.println("</body></html>");
   }
   
   @RequestMapping("/board/update")
-  public void update(ServletResponse response) throws Exception {
-    Board temp = new Board();
-    temp.setNo(response.requestInt("번호?"));
+  public void update(ServletRequest request, ServletResponse response) throws Exception {
     
-    String input = response.requestString("내용?");
-    if (input.length() > 0)
-      temp.setContents(input);
+    Board board = new Board();
+    board.setNo(Integer.parseInt(request.getParameter("no")));
+    board.setContents(request.getParameter("contents"));
     
-    if (temp.getContents() != null) {
-      if (boardService.update(temp) == 0) {
-        response.println("해당 번호의 게시물이 없습니다.");
-        return;
-      }
-      response.println("변경했습니다.");
-      
-    } else {
-      response.println("변경 취소했습니다.");
+    PrintWriter out = response.getWriter();
+    out.println("<html><head>"
+        + "<title>게시물 변경</title>"
+        + "<meta http-equiv='Refresh' content='1;url=/board/list'>"
+        + "</head>");
+    out.println("<body><h1>게시물 변경</h1>");
+    
+    if (boardService.update(board) == 0) {
+      out.println("<p>해당 번호의 게시물이 없습니다.</p>");
+    } else { 
+      out.println("<p>변경했습니다.</p>");
     }
+    
+    out.println("</body></html>");
   }
   
   @RequestMapping("/board/delete")
-  public void delete(ServletResponse response) throws Exception {
-    int no = response.requestInt("번호?");
+  public void delete(ServletRequest request, ServletResponse response) throws Exception {
+    
+    int no = Integer.parseInt(request.getParameter("no"));
 
+    PrintWriter out = response.getWriter();
+    out.println("<html><head>"
+        + "<title>게시물 삭제</title>"
+        + "<meta http-equiv='Refresh' content='1;url=/board/list'>"
+        + "</head>");
+    out.println("<body><h1>게시물 삭제</h1>");
+    
     if (boardService.delete(no) == 0) {
-      response.println("해당 번호의 게시물이 없습니다.");
-      return;
+      out.println("<p>해당 번호의 게시물이 없습니다.</p>");
+    } else { 
+      out.println("<p>삭제했습니다.</p>");
     }
-    response.println("삭제했습니다.");
+    
+    out.println("</body></html>");
   }
   
   @RequestMapping("/board/form")
--- java-project2-server/src/main/java/com/eomcs/lms/handler/ServletRequest.java
+++ java-project2-server/src/main/java/com/eomcs/lms/handler/ServletRequest.java
@@ -1,5 +1,6 @@
 package com.eomcs.lms.handler;
 
+import java.net.URLDecoder;
 import java.util.HashMap;
 
 public class ServletRequest {
@@ -9,7 +10,12 @@ public class ServletRequest {
     String[] params = qs.split("&"); // 예)name=aaa&email=aaa@test.com
     for (String param : params) {
       String[] values = param.split("="); // 예) name=aaa
-      paramMap.put(values[0], values[1]);
+      try { 
+        // 웹 브라우저가 보낸 값을 보관할 때 URL 디코딩 하여 보관한다. 
+        paramMap.put(values[0], URLDecoder.decode(values[1], "UTF-8"));
+      } catch (Exception e) {
+        // URL 인코딩 데이터를 디코딩 하다가 오류가 발생하면 무시한다.
+      }
     }
   }
   
