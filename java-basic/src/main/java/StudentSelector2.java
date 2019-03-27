b0955884d0bc9a31485998040691a0d9198023fd
eomjinyoung <jinyoung.eom@gmail.com>
2019-03-27 15:08:24 +0900

    .

--- java-basic/src/main/java/StudentSelector2.java
+++ java-basic/src/main/java/StudentSelector2.java
@@ -1,9 +1,15 @@
 import java.util.ArrayList;
 
-public class StudentSelector {
+public class StudentSelector2 {
 
   public static void main(String[] args) throws Exception {
-    
+    String[] names = {
+        "김기원", "김시진", "김의주", "김지성", "한성우",
+        "김화선", "박상민", "박재중", "이대구", "임현우",
+        "전민희", "전상오", "전진욱", "최미영", "최지환"
+    };
+
+    /*
     String[] names = {
         "김기원", "김민철", "김시진", "김의주", "김지성", "김혜인",
         "김화선", "류재현", "박상민", "박상현", "박재중", "손병준",
@@ -11,22 +17,23 @@ public class StudentSelector {
         "전민희", "전상오", "전진욱", "지희욱", "최미영", "최익현",
         "최지환", "한성우"
     };
-    
+     */
+
     ArrayList<String> list = new ArrayList<>();
     for (String name : names) {
       list.add(name);
     }
-    
+
     int no = -1;
-    for (int i = 0; i < 10; i++) {
-      no = (int)(Math.random() * names.length);
-      System.out.print(".");
-      Thread.currentThread().sleep(200);
+    while (list.size() > 0) {
+      for (int i = 0; i < 10; i++) {
+        no = (int)(Math.random() * list.size());
+        System.out.print(".");
+        Thread.currentThread().sleep(300);
+      }
+      Thread.currentThread().sleep(7000);
+      System.out.println(list.remove(no));
     }
-    System.out.println();
-    
-    Thread.currentThread().sleep(5000);
-    System.out.println(names[no]);
 
   }
 
---
+++ java-web/README.md
@@ -0,0 +1,28 @@
+# 서블릿 프로그래밍
+
+## 자바 웹 프로젝트 만들기
+- bitcamp-2018-12/java-web 디렉토리 생성
+- '$ gradle init' 실행
+- java-web/src/main/webapp 디렉토리 생성
+- webapp/index.html 생성 및 편집
+- build.gradle 파일 편집
+    - 'application' 플러그인 제거 및 그와 관계된 설정 제거
+    - 'eclipse-wtp', 'war' 플러그인 추가
+    - servlet-api 라이브러리 추가
+- 이클립스 IDE 용 설정 파일 생성
+    - '$ gradle eclipse' 실행 
+- 이클립스 IDE로 프로젝트 임포트 
+
+## 자바 웹 프로젝트 배치와 실행
+- 톰캣 테스트 환경 추가
+    - 'Servers' 뷰에서 테스트로 사용할 Tomcat 환경 추가
+- 톰캣 테스트 환경에 java-web 프로젝트 배치
+    - 'Servers' 뷰/java-web 테스트 환경을 선택
+    - 컨텍스트 메뉴 'Add and Remove...' 선택
+    - java-web 프로젝트를 추가 
+- 톰캣 테스트 환경 실행
+    - 테스트 서버에 대해 컨텍스트 메뉴에서 Start 클릭
+    - 이클립스워크스페이스폴더/.metadata/.plugins/org.eclipse.wst.server.core/tmp1/wtpwebapps/java-web 디렉토리 확인
+- 웹 브라우저에서 톰캣 서버 접속 확인
+    - http://localhost:8080/java-web 주소를 입력한 후 결과 확인
+    
