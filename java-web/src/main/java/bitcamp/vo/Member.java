b96c58399946c472ac2eaabe8dbfbd4aaa535336
eomjinyoung <jinyoung.eom@gmail.com>
2019-04-09 10:43:21 +0900

    .

---
+++ java-web/src/main/java/bitcamp/vo/Member.java
@@ -0,0 +1,41 @@
+package bitcamp.vo;
+
+public class Member {
+  protected int no;
+  protected String name;
+  protected String email;
+  protected String tel;
+  protected String password;
+  
+  public int getNo() {
+    return no;
+  }
+  public void setNo(int no) {
+    this.no = no;
+  }
+  public String getName() {
+    return name;
+  }
+  public void setName(String name) {
+    this.name = name;
+  }
+  public String getEmail() {
+    return email;
+  }
+  public void setEmail(String email) {
+    this.email = email;
+  }
+  public String getTel() {
+    return tel;
+  }
+  public void setTel(String tel) {
+    this.tel = tel;
+  }
+  public String getPassword() {
+    return password;
+  }
+  public void setPassword(String password) {
+    this.password = password;
+  }
+  
+}
