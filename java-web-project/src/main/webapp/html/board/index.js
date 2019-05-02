16d8ff1123e425c517d159696fdc06cb11561c57
eomjinyoung <jinyoung.eom@gmail.com>
2019-05-02 11:02:51 +0900

    2019-5-2

---
+++ java-web-project/src/main/webapp/html/board/index.js
@@ -0,0 +1,103 @@
+var pageNo = 1,
+    pageSize = 3,
+    header = document.querySelector('body > header'),
+    tbody = document.querySelector('tbody'),
+    prevPageLi = document.querySelector('#prevPage'),
+    nextPageLi = document.querySelector('#nextPage'),
+    currSpan = document.querySelector('#currPage > span');
+
+// 헤더 가져오기
+(function () {
+  var xhr = new XMLHttpRequest()
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState != 4 || xhr.status != 200)
+      return;
+    header.innerHTML = xhr.responseText
+    
+    // body 태그 쪽에 헤더 로딩 완료 이벤트를 보낸다.
+    //var e = new Event("loaded.header");
+    //document.body.dispatchEvent(e);
+  };
+  xhr.open('GET', '../header.html', true)
+  xhr.send()
+})();
+
+/* 헤더가 로딩이 완료된 다음에 테이블에 출력할 데이터를 가져오고 싶다면 다음 코드를 참고하라!
+// 헤더 로딩이 완료되었을 때 호출될 함수 등록
+document.body.addEventListener('loaded.header', () => {
+  // 페이지의 헤더를 출력한 후 1페이지 목록을 로딩한다.
+  loadList(1);
+});
+*/
+
+// JSON 형식의 데이터 목록 가져오기
+function loadList(pn) {
+  var xhr = new XMLHttpRequest()
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState != 4 || xhr.status != 200)
+      return;
+    
+    // 서버에서 받은 JSON 형식의 데이터를 ==> JavaScript 객체로 변환
+    var obj = JSON.parse(xhr.responseText);
+   
+    // 서버에 받은 데이터 중에서 페이지 번호를 글로벌 변수에 저장한다.
+    pageNo = obj.pageNo;
+    
+    // TR 태그를 생성하여 테이블 데이터를 갱신한다.
+    tbody.innerHTML = ''; // 이전에 출력한 내용을 제거한다.
+    for (data of obj.list) {
+      var tr = '<tr>'
+        + '<th scope="row">' + data.no + '</th>'
+        + '<td><a href="' + data.no + '">' + data.contents + '</a></td>'
+        + '<td>' + data.createdDate + '</td>'
+        + '<td>' + data.viewCount + '</td>'
+        + '</tr>';
+      tbody.innerHTML = tbody.innerHTML + tr;
+    }
+    
+    // 현재 페이지의 번호를 갱신한다.
+    currSpan.innerHTML = String(pageNo)
+    
+    // 1페이지일 경우 버튼을 비활성화 한다.
+    if (pageNo == 1) {
+      prevPageLi.className = prevPageLi.className + ' disabled';
+    } else {
+      prevPageLi.className = prevPageLi.className.replace(' disabled', '');
+    } 
+      
+    // 마지막 페이지일 경우 버튼을 비활성화 한다.
+    if (pageNo == obj.totalPage) {
+      nextPageLi.className = nextPageLi.className + ' disabled';
+    } else {
+      nextPageLi.className = nextPageLi.className.replace(' disabled', '');
+    }
+    
+  };
+  xhr.open('GET', 
+      '../../app/json/board/list?pageNo=' + pn + '&pageSize=' + pageSize, 
+      true)
+  xhr.send()
+} // loadList()
+
+document.querySelector('#prevPage > a').onclick = (e) => {
+  e.preventDefault();
+  loadList(pageNo - 1);
+};
+
+document.querySelector('#nextPage > a').onclick = (e) => {
+  e.preventDefault();
+  loadList(pageNo + 1);
+};
+
+//페이지를 출력한 후 1페이지 목록을 로딩한다.
+loadList(1);
+
+
+
+
+
+
+
+
+
+
