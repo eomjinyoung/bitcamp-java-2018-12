70aef92bf9c799d3dde245f593d75aefd978494b
eomjinyoung <jinyoung.eom@gmail.com>
2019-05-08 12:16:03 +0900

    2019-5-8

--- java-web-project/src/main/webapp/html/board/index.js
+++ java-web-project/src/main/webapp/html/board/index.js
@@ -16,14 +16,16 @@ function loadList(pn) {
       // TR 태그를 생성하여 테이블 데이터를 갱신한다.
       tbody.html(''); // 이전에 출력한 내용을 제거한다.
       for (data of obj.list) {
-        var tr = '<tr>'
-          + '<th scope="row">' + data.no + '</th>'
-          //+ '<td><a href="view.html?no=' + data.no + '">' + data.contents + '</a></td>'
-          + '<td><a class="bit-view-link" href="#" data-no="' + data.no + '">' + data.contents + '</a></td>'
-          + '<td>' + data.createdDate + '</td>'
-          + '<td>' + data.viewCount + '</td>'
-          + '</tr>';
-        tbody.html(tbody.html() + tr);
+        $('<tr>')
+          .append($('<th>').attr('scope', 'row').html(data.no))
+          .append($('<td>').append(
+                    $('<a>').addClass('bit-view-link')
+                      .attr('href', '#')
+                      .attr('data-no', data.no)
+                      .html(data.contents)))
+          .append($('<td>').html(data.createdDate))
+          .append($('<td>').html(data.viewCount))
+          .appendTo(tbody);
       }
       
       // 현재 페이지의 번호를 갱신한다.
@@ -44,36 +46,34 @@ function loadList(pn) {
       }
       
       // 데이터 로딩이 완료되면 body 태그에 이벤트를 전송한다.
-      document.body.dispatchEvent(new Event('loaded-list'));
+      $(document.body).trigger('loaded-list');
+      
     }); // Bitcamp.getJSON()
   
 } // loadList()
 
-document.querySelector('#prevPage > a').onclick = (e) => {
+$('#prevPage > a').click((e) => {
   e.preventDefault();
   loadList(pageNo - 1);
-};
+});
 
-document.querySelector('#nextPage > a').onclick = (e) => {
+$('#nextPage > a').click((e) => {
   e.preventDefault();
   loadList(pageNo + 1);
-};
+});
 
 
 //페이지를 출력한 후 1페이지 목록을 로딩한다.
 loadList(1);
 
 // 테이블 목록 가져오기를 완료했으면 제목 a 태그에 클릭 리스너를 등록한다. 
-document.body.addEventListener('loaded-list', () => {
+$(document.body).bind('loaded-list', () => {
   // 제목을 클릭했을 때 view.html로 전환시키기
-  var alist = document.querySelectorAll('.bit-view-link');
-  for (a of alist) {
-    a.onclick = (e) => {
-      e.preventDefault();
-      window.location.href = 'view.html?no=' + 
-        e.target.getAttribute('data-no');
-    };
-  }
+  $('.bit-view-link').click((e) => {
+    e.preventDefault();
+    window.location.href = 'view.html?no=' + 
+      $(e.target).attr('data-no');
+  });
 });
 
 
--- java-web-project/src/main/webapp/js/bitcamp.js
+++ java-web-project/src/main/webapp/js/bitcamp.js
@@ -1,10 +1,22 @@
 var Bitcamp = function(arg1) { // function = object + 함수 코드
-  var el = document.querySelectorAll(arg1); // => HTMLCollection 리턴
-  
-  // HTMLCollection에 들어 있는 값을 빈 배열에 옮긴다.
   var arr = [];
-  for (var e of el) {
-    arr.push(e);
+
+  if (arg1 instanceof HTMLElement) {
+    // 파라미터 값이 태그 객체라면 그대로 배열에 저장한다.
+    arr.push(arg1);
+    
+  } else if (arg1.startsWith('<')) {
+    // 파라미터 값이 태그를 의미하는 문자열이라면 해당 태그를 만든 후 배열에 저장한다.
+    var tagName = arg1.substr(1, arg1.length - 2);
+    arr.push(document.createElement(tagName)); 
+    
+  } else {
+    var el = document.querySelectorAll(arg1); // => HTMLCollection 리턴
+  
+    // HTMLCollection에 들어 있는 값을 빈 배열에 옮긴다.
+    for (var e of el) {
+      arr.push(e);
+    }
   }
   
   // 태그가 들어있는 배열에 그 태그들을 다룰 때 사용할 도구를 첨가한다.
@@ -13,6 +25,7 @@ var Bitcamp = function(arg1) { // function = object + 함수 코드
       for (var e of this) {
         e.innerHTML = value;
       }
+      return this;
     } else {
       // 찾은 태그 중에서 첫 번째 태그의 innerHTML 값만 리턴한다.
       return this[0].innerHTML;
@@ -22,16 +35,86 @@ var Bitcamp = function(arg1) { // function = object + 함수 코드
   // 태그에 클래스 이름을 추가하는 함수를 배열에 첨부한다.
   arr.addClass = function(value) {
     for (var e of this) {
-      e.className = e.className + " " + value;
+      var cn = value;
+      var names = e.className.split(' ');
+      for (var name of names) {
+        cn = cn + ' ' + name;
+      }
+      e.className = cn;
     }
+    return this;
   };
   
   // 태그에 클래스 이름을 제거하는 함수를 배열에 첨부한다.
   arr.removeClass = function(value) {
     for (var e of this) {
-      e.className = e.className.replace(value, '');
+      var cn = '';
+      var names = e.className.split(' ');
+      for (var name of names) {
+        if (name == value)
+          continue;
+        cn = cn + ' ' + name;
+      }
+      e.className = cn;
     }
+    return this;
   }
+  
+  // click 리스너를 등록하는 함수를 배열에 첨부한다.
+  arr.click = function(cb) {
+    for (var e of this) {
+      e.addEventListener('click', cb);
+    }
+    return this;
+  };
+  
+  // 특정 이벤트의 리스너를 등록하는 함수를 배열에 첨부한다.
+  arr.bind = function(eventType, handler) {
+    for (var e of arr) {
+      e.addEventListener(eventType, handler);
+    }
+    return this;
+  };
+  
+  // 태그의 속성 값을 설정하고 리턴하는 함수를 배열에 첨부한다.
+  arr.attr = function(name, value) {
+    if (arguments.length == 1) {
+      return this[0].getAttribute(name);
+    } else {
+      for (var e of this) {
+        e.setAttribute(name, value);
+      }
+      return this;
+    }
+  };
+  
+  // 특정 태그에 이벤트를 발생시키는 함수를 배열에 첨부한다.
+  arr.trigger = function(eventType) {
+    for (var e of this) {
+      e.dispatchEvent(new Event(eventType));
+    }
+    return this;
+  };
+  
+  // 태그에 자식 태그를 추가하는 함수를 배열에 첨부한다.
+  arr.append = function(childs) {
+    for (var parent of this) {
+      for (var child of childs) {
+        parent.appendChild(child);
+      }
+    }
+    return this;
+  };
+  
+  // 태그를 부모 태그에 추가하는 함수를 배열에 첨부한다.
+  arr.appendTo = function(parents) {
+    for (var parent of parents) {
+      for (var child of this) {
+        parent.appendChild(child);
+      }
+    }
+    return this;
+  };
   
   return arr;
 }; 
